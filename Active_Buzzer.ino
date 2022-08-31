7ab67f3b36f361aa3735fde82c13dafc02bbc6e8
Julieta Rojas García <111190618+julietarojas@users.noreply.github.com>
2022-08-31 14:30:00 -0500

    Add files via upload

---
+++ Active_Buzzer.ino
@@ -0,0 +1,39 @@
+//www.elegoo.com
+//2016.12.08
+
+/*by:Julieta Rojas*/
+
+//we declare the pin of the active buzzer
+int buzzer = 12;
+
+void setup()
+{
+  //we initialize the buzzer pin as an output
+ pinMode(buzzer,OUTPUT);
+}
+
+
+void loop()
+{
+ unsigned char i;
+ while(1)
+ {
+   //output an frequency
+   for(i=0;i<80;i++)
+   {
+    digitalWrite(buzzer,HIGH);
+    delay(1);//wait for 1ms
+    digitalWrite(buzzer,LOW);
+    delay(1);//wait for 1ms
+    }
+    
+    //output another frequency
+     for(i=0;i<100;i++)
+      {
+        digitalWrite(buzzer,HIGH);
+        delay(2);//wait for 2ms
+        digitalWrite(buzzer,LOW);
+        delay(2);//wait for 2ms
+      }
+  }
+} 
